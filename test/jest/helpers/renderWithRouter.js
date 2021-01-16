afd685937a5cd256611f6695f151e4768cbcb284
Viktor Soroka <viktor_soroka@epam.com>
2021-01-16 19:16:31 +0200

    UITEN-146: Update `stripes` to `v6.0.0` (#183)

--- test/jest/helpers/renderWithRouter.js
+++ test/jest/helpers/renderWithRouter.js
@@ -1,7 +1,6 @@
-
 import React from 'react';
-import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
 
 const renderWithRouter = (children) => render(
   <MemoryRouter>{children}</MemoryRouter>
