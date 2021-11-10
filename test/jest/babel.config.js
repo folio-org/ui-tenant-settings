bca9d957b389328652ae6a35f4a28c388e38e158
uladzislau_kutarkin <uladzislau_kutarkin@epam.com>
2021-11-10 21:00:48 +0300

    change jest setups

--- test/jest/babel.config.js
+++ test/jest/babel.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-  presets: [
-    '@babel/preset-env',
-    ['@babel/preset-react', { 'runtime': 'automatic' }],
-  ],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
     '@babel/plugin-transform-runtime',
   ],
 };
