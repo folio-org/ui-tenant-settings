6c65ca4f891e3a53e08d5951b90525b82ba38b95
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2023-06-23 10:57:39 +0400

    UITEN-233: fix comments

--- test/jest/babel.config.js
+++ test/jest/babel.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ],
   plugins: [
     ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-    ['@babel/plugin-transform-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-transform-class-properties', { loose: true }],
     ['@babel/plugin-transform-private-methods', { loose: true }],
     '@babel/plugin-transform-runtime',
