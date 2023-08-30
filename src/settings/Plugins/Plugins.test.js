5636793ab4f1b1f2992ef70258cf3e0c7ac8a4f9
vashjs <vadym.shchekotilin@gmail.com>
2023-08-30 22:09:43 +0300

    update tests

--- src/settings/Plugins/Plugins.test.js
+++ src/settings/Plugins/Plugins.test.js
@@ -12,10 +12,12 @@ import {
 import Plugins from './Plugins';
 
 const setSinglePlugin = jest.fn();
+const hasPermMock = jest.fn().mockReturnValue(true);
 
 const ConnectedPlugins = stripesConnect(Plugins, {
   stripes: {
     setSinglePlugin,
+    hasPerm: hasPermMock
   },
 });
 
@@ -24,6 +26,7 @@ const renderPlugins = (props) => renderWithRouter(<ConnectedPlugins {...props} /
 describe('Plugins', () => {
   afterEach(() => {
     setSinglePlugin.mockClear();
+    hasPermMock.mockClear();
   });
 
   it('should render plugins', async () => {
@@ -38,5 +41,13 @@ describe('Plugins', () => {
     user.click(screen.getByRole('button', { type: /submit/i }));
 
     expect(setSinglePlugin).toHaveBeenCalledTimes(1);
+  });
+
+  it('submit button should not be present without permissions', async () => {
+    hasPermMock.mockReturnValueOnce(false);
+
+    renderPlugins({ label: 'plugins' });
+
+    expect(screen.queryByRole('button', { type: /submit/i })).toBeNull();
   });
 });
