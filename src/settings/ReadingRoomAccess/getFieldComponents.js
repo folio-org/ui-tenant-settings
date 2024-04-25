801a0ce286fa455f02ae0bc9b3558728202b93bc
Priyanka Terala <priyanka_terala@epam.com>
2024-04-25 11:46:41 +0530

    UITEN-278 - fix prop types sonar problem

--- src/settings/ReadingRoomAccess/getFieldComponents.js
+++ src/settings/ReadingRoomAccess/getFieldComponents.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Field } from 'react-final-form';
 
 import {
@@ -8,39 +9,48 @@ import {
 
 import { readingRoomAccessColumns } from './constant';
 
-/* eslint-disable react/prop-types, import/prefer-default-export */
+/* eslint-disable import/prefer-default-export */
 export const getFieldComponents = (fieldLabels, options) => ({
-  [readingRoomAccessColumns.NAME]: ({ fieldProps, name, rowIndex, fieldIndex }) => (
-    <Field
-      {...fieldProps}
-      component={TextField}
-      aria-label={`${fieldLabels[name]} ${rowIndex}`}
-      placeholder={fieldLabels[name]}
-      marginBottom0
-      autoFocus={fieldIndex === 0}
-    />
+  [readingRoomAccessColumns.NAME]: Object.assign(
+    ({ fieldProps, name, rowIndex, fieldIndex }) => (
+      <Field
+        {...fieldProps}
+        component={TextField}
+        aria-label={`${fieldLabels[name]} ${rowIndex}`}
+        placeholder={fieldLabels[name]}
+        marginBottom0
+        autoFocus={fieldIndex === 0}
+      />
+    ),
+    { propTypes: { fieldProps: PropTypes.object, name: PropTypes.string, rowIndex: PropTypes.number, fieldIndex: PropTypes.number } }
   ),
-  [readingRoomAccessColumns.ISPUBLIC]: ({ fieldProps, name, rowIndex }) => (
-    <Field
-      {...fieldProps}
-      aria-label={`${fieldLabels[name]} ${rowIndex}`}
-      component={Checkbox}
-      initialValue={false}
-      type="checkbox"
-      marginBottom0
-    />
+  [readingRoomAccessColumns.ISPUBLIC]: Object.assign(
+    ({ fieldProps, name, rowIndex }) => (
+      <Field
+        {...fieldProps}
+        aria-label={`${fieldLabels[name]} ${rowIndex}`}
+        component={Checkbox}
+        initialValue={false}
+        type="checkbox"
+        marginBottom0
+      />
+    ),
+    { propTypes: { fieldProps: PropTypes.object, name: PropTypes.string, rowIndex: PropTypes.number } }
   ),
-  [readingRoomAccessColumns.SERVICEPOINTS]: ({ fieldProps, name, rowIndex }) => (
-    <Field
-      {...fieldProps}
-      id="rra-service-point"
-      component={MultiSelection}
-      aria-label={`${fieldLabels[name]} ${rowIndex}`}
-      dataOptions={options}
-      renderToOverlay
-      marginBottom0
-      validationEnabled
-      onBlur={e => e.preventDefault()}
-    />
+  [readingRoomAccessColumns.SERVICEPOINTS]: Object.assign(
+    ({ fieldProps, name, rowIndex }) => (
+      <Field
+        {...fieldProps}
+        id="rra-service-point"
+        component={MultiSelection}
+        aria-label={`${fieldLabels[name]} ${rowIndex}`}
+        dataOptions={options}
+        renderToOverlay
+        marginBottom0
+        validationEnabled
+        onBlur={e => e.preventDefault()}
+      />
+    ),
+    { propTypes: { fieldProps: PropTypes.object, name: PropTypes.string, rowIndex: PropTypes.number } }
   )
 });
--- src/settings/ReadingRoomAccess/getValidators.js
+++ src/settings/ReadingRoomAccess/getValidators.js
@@ -3,16 +3,10 @@ import { FormattedMessage } from 'react-intl';
 import { readingRoomAccessColumns } from './constant';
 
 const validators = {
-  [readingRoomAccessColumns.NAME]: function validateName(item, items) {
-    const { id, name } = item;
+  [readingRoomAccessColumns.NAME]: function validateName(item) {
+    const { name } = item;
     if (!name) {
       return <FormattedMessage id="stripes-core.label.missingRequiredField" />;
-    }
-
-    const allOtherNames = items.filter(r => r.id !== id).map(r => r.name);
-
-    if (allOtherNames.some(itemName => itemName === name)) {
-      return <FormattedMessage id="ui-marc-authorities.settings.manageAuthoritySourceFiles.error.name.unique" />;
     }
 
     return undefined;
